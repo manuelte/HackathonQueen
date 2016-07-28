d3bd0c8d31103504156b1778573c744119696028
Manuel Tenorio <manuelte@microsoft.com>
2016-07-28 11:35:24 -0700

    Added Laurel's menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
@@ -16,15 +16,12 @@ public class Menu : MonoBehaviour {
         buttonStyle.normal.textColor = Color.black;
         buttonStyle.fontSize = 40;
         buttonStyle.alignment = TextAnchor.MiddleCenter;
-        buttonStyle.hover.textColor = Color.blue; // Will not show up until background image is added
 
         GUI.Box(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 235, 400, 100), "#QUEEN", titleStyle);
-        if (GUI.Button(new Rect(Screen.width / 2 - 300, Screen.height / 2 - 25, 200, 75), "Start", buttonStyle))
-        {
-            // Add in code to call next scene: GetComponent<NAME>().enabled = true;
-            this.enabled = false;
-        }
-        if (GUI.Button(new Rect(Screen.width / 2 - 300, Screen.height / 2 + 75, 200, 75), "Quit", buttonStyle))
+        if (GUI.Button(new Rect(Screen.width / 2 - 300, Screen.height / 2 - 50, 200, 75), "Start", buttonStyle))
+            SceneManager.LoadScene("Level01");
+        
+        if (GUI.Button(new Rect(Screen.width / 2 - 300, Screen.height / 2 + 50, 200, 75), "Quit", buttonStyle))
             Application.Quit();
     }
 }
